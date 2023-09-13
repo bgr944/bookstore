648e581304e570bb22c453d1bd0f249fced248c1
bgr944 <bgr944@myy.haaga-helia.fi>
2023-09-13 15:52:01 +0300

    template for real this time

--- src/main/java/hh/sof03/Bookstore/domain/Book.java
+++ src/main/java/hh/sof03/Bookstore/domain/Book.java
@@ -2,4 +2,74 @@ package hh.sof03.Bookstore.domain;
 
 public class Book {
 
+	 public String title;
+	    
+	    public String author;
+	    
+	    public int year;
+	    
+	    public String isbn;
+	    
+	    public double price;
+	    
+	    
+	    public Book(String title,String author,int year,String isbn,double price)
+	    {   this.title=title;
+	        this.author=author;
+	        this.year=year;
+	        this.isbn=isbn;
+	        this.price=price;
+	    }
+	    
+	      
+	    public String getTitle()
+	    {
+	        return title;
+	    }
+	    
+	     public String getAuthor()
+	    {
+	        return author;
+	    }
+	    public int getYear()
+	    {
+	        return year;
+	    }
+	    public String getIsbn()
+	    {
+	        return isbn;
+	    } 
+	    public double getPrice()
+	    {
+	        return price;
+	    }
+
+	    public void setTitle(String title)
+	    {
+	        this.title=title;
+	    }
+	    
+	     public void setAuthor(String author)
+	    {
+	       this.author=author;
+	    }
+	    public void setYear(int year)
+	    {
+	        this.year=year;
+	    }
+	    public void setIsbn(String isbn)
+	    {
+	        this.isbn=isbn;
+	    } 
+	    public void setPrice(double price)
+	    {
+	        this.price=price;
+	    }    
+	    
+	    
+	    public String toString()
+	    {
+	        return  title + ", " + author + ", " + year + ", " + isbn + ", " + price;
+	    }
+	
 }
--- src/main/java/hh/sof03/Bookstore/web/BookstoreController.java
+++ src/main/java/hh/sof03/Bookstore/web/BookstoreController.java
@@ -1,5 +1,26 @@
 package hh.sof03.Bookstore.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import hh.sof03.Bookstore.domain.Book;
+
+@Controller
 public class BookstoreController {
 
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String Books(Model model) {
+		
+		List<Book> Books = new ArrayList<Book>();
+
+		model.addAttribute("Books", Books);
+		
+		return index;
+		
+    }
 }
